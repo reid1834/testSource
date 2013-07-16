b611568691f707118cd716ebab90d046c679acc1
unknown <yang.li2@RH-F0486-SH1SW.ragent.cn>
2013-07-16 10:05:25 +0800

    add netimage

---
+++ netimage/src/com/reid/service/ImageService.java
@@ -0,0 +1,29 @@
+package com.reid.service;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.reid.utils.StreamTool;
+
+public class ImageService {
+
+	/**
+	 * 获取网络图片的数据
+	 * @param path 网络图片路径
+	 * @return
+	 */
+	public static byte[] getImage(String path) throws Exception{
+		URL url = new URL(path);
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();//基于HTTP协议的连接对象
+		conn.setConnectTimeout(5000);
+		conn.setRequestMethod("GET");
+		if(conn.getResponseCode() == 200){
+			InputStream inStream = conn.getInputStream();
+			return StreamTool.read(inStream);
+		}
+		
+		return null;
+	}
+
+}
