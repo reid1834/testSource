213800bf6228a7cf306991030b707ed3c9bcaa21
unknown <yang.li2@RH-F0486-SH1SW.ragent.cn>
2013-07-26 09:53:34 +0800

    modify

---
+++ netimage/gen/com/reid/netimage/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.reid.netimage;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- netimage/src/com/reid/netimage/MainActivity.java
+++ netimage/src/com/reid/netimage/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -24,9 +23,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        StrictMode.setThreadPolicy(new 
-        StrictMode.ThreadPolicy.Builder().detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
-        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectLeakedSqlLiteObjects().detectLeakedClosableObjects().penaltyLog().penaltyDeath().build());
+//        StrictMode.setThreadPolicy(new 
+//        StrictMode.ThreadPolicy.Builder().detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
+//        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectLeakedSqlLiteObjects().detectLeakedClosableObjects().penaltyLog().penaltyDeath().build());
         
         pathText = (EditText)findViewById(R.id.imagepath);
         imageView = (ImageView)findViewById(R.id.imageView);
@@ -39,6 +38,7 @@ public class MainActivity extends Activity {
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
 			String path = pathText.getText().toString();
+
 			try {
 				byte[] data = ImageService.getImage(path);
 				Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
